d433a0959aae065623d5a0b5a1f7dde97cf9e30e
Habib Bouzoffara <habib.bouzaffara@ieee.org>
2024-07-20 00:50:37 +0100

    Final

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -90,7 +90,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
           autoRotate
-          autoRotateSpeed={0.5}
+          autoRotateSpeed={0.1}
         />
         <Computers isMobile={isMobile} />
       </Suspense>
